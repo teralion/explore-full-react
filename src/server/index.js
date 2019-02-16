a8092e9b6dfa5db13c42330e9ed4023c1b48436a
teralion <slavad878@gmail.com>
2019-02-16 09:27:04 +0300

    User process.env.PORT

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
+app.set('port', process.env.PORT || 8080);
 app.get('/api/username', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(8080, () => console.log('App is running on 8080 port'));
+app.listen(app.get('port'), () => console.log('App is running on ' + server.address().port + ' port'));
